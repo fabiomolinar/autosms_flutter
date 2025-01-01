1ea7f759cef183a35e2ac43e450a859e15162abe
Fabio Thomaz Molinar <fabiomolinar@gmail.com>
2025-01-01 22:37:39 +0100

    Updated.

--- autosms/macos/Flutter/GeneratedPluginRegistrant.swift
+++ autosms/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import google_sign_in_ios
+import msal_auth
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  MsalAuthPlugin.register(with: registry.registrar(forPlugin: "MsalAuthPlugin"))
 }
